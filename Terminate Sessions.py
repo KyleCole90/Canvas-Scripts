ca40e3f82d7cf85355231547e5a8704627c14e1f
Kyle Cole <kyleedwardcole@gmail.com>
2023-09-25 19:26:34 -0400

    Update Terminate Sessions.py

--- Terminate Sessions.py
+++ Terminate Sessions.py
@@ -1,28 +1,33 @@
 import requests
 import csv
 
-key = "Bearer " ### API Token
-canvas = " " ### Canvas Domain 
-id = open ('user.csv', encoding='utf-8-sig') ##### Pull Users.CSV and name it user.csv
-print(id)
-domain = csv.reader(id)
-print(domain)
-###### Do not edit below#### 
+# Define your API token, Canvas domain, and the path to the CSV file
+auth_token = "YOUR_API_TOKEN"
+canvas_domain = "YOUR_CANVAS_DOMAIN"
+csv_file_path = 'user.csv'
 
-headers = {
-    'Authorization': key
-}
-
-print(id)
-domain = csv.reader(id)
-print(domain)
-
-for row in domain:
-    id=(row[0])
-    url = canvas + "/api/v1/users/" + id + "/sessions"
-    print(url)
-    headers = {
-        'Authorization': key
-    }
-    response = requests.request("Delete",url,headers=headers)
-    print(response)
+# Open the CSV file and create a CSV reader
+with open(csv_file_path, mode='r', encoding='utf-8-sig') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    
+    # Skip the header row if it exists
+    next(csv_reader, None)
+    
+    # Loop through the CSV rows
+    for row in csv_reader:
+        user_id = row[0]
+        url = f"{canvas_domain}/api/v1/users/{user_id}/sessions"
+        
+        # Create the headers dictionary with the API token
+        headers = {
+            'Authorization': f"Bearer {auth_token}"
+        }
+        
+        # Send a DELETE request
+        response = requests.delete(url, headers=headers)
+        
+        # Check the response status code and print the result
+        if response.status_code == 200:
+            print(f"Session for user ID {user_id} deleted successfully.")
+        else:
+            print(f"Failed to delete session for user ID {user_id}. Status code: {response.status_code}")
